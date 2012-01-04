dd97cc2501fe3a7d68f7ccd3b3056c19bee36932
mchmurski <m.chmurski@gmail.com>
2012-01-04 20:38:38 +0100

    Improved About page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spork'
+
+Spork.prefork do
+# Loading more in this block will cause your tests to run faster. However, 
+# if you change any configuration or code from libraries loaded here, you'll
+# need to restart spork for it take effect.
+end
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
