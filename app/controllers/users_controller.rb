c52d177ed84443db678a0ed272a72c6e87d08211
mchmurski <m.chmurski@gmail.com>
2012-01-25 13:26:52 +0100

    done with user sign-up failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:id])
+    @user = User.new(params[:user])
     if @user.save
       #Handle a successful save.
     else
